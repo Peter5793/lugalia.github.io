0459ca26bb4b8393270e9c43e03e3034ef1d64c2
Peter Lugalia <p.lugalia@launchpad.alx.app>
2023-01-01 20:29:22 +0100

    place rset in js files and edit html

--- version 2.0/MyResume/MyResume/assets/js/main.js
+++ version 2.0/MyResume/MyResume/assets/js/main.js
@@ -266,4 +266,19 @@
    */
   new PureCounter();
 
+  var form = document.getElementById('sheetdb-form');
+form.addEventListener("submit", e => {
+  e.preventDefault();
+  fetch(form.action, {
+      method : "POST",
+      body: new FormData(document.getElementById("sheetdb-form")),
+  }).then(
+      response => response.json()
+  ).then((html) => {
+    // you can put any JS code here
+    alert('Awesome you are a Super Star 😎')
+    form.reset()
+  });
+});
+
 })()
--- version 2.0/MyResume/MyResume/assets/vendor/php-email-form/validate.js
+++ version 2.0/MyResume/MyResume/assets/vendor/php-email-form/validate.js
@@ -78,4 +78,19 @@
     thisForm.querySelector('.error-message').classList.add('d-block');
   }
 
+  var form = document.getElementById('sheetdb-form');
+
+  form.addEventListener("submit", e => {
+  e.preventDefault();
+  fetch(form.action, {
+      method : "POST",
+      body: new FormData(document.getElementById("sheetdb-form")),
+  }).then(
+      response => response.json()
+  ).then((html) => {
+    // you can put any JS code here
+    
+  });
+});
+
 })();
